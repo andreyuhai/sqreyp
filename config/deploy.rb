49604f3003c69e18648a83c9f99793d6f37f12d1
andreyuhai <burak.kaymakci@bil.omu.edu.tr>
2020-02-23 00:01:19 +0300

    Typo fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock "~> 3.12.0"
 
 set :application, "sqreyp"
-set :repo_url, "ggit@github.com:andreyuhai/sqreyp.git"
+set :repo_url, "git@github.com:andreyuhai/sqreyp.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
