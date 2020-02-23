da7944b05fc5a9141a02f88e9c8afbcd44a87b17
andreyuhai <burak.kaymakci@bil.omu.edu.tr>
2020-02-23 13:26:25 +0300

    Linked dirs changed

--- config/deploy.rb
+++ config/deploy.rb
@@ -29,7 +29,7 @@ append :linked_files, "config/master.key"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-append :linked_dirs, "log", "public/system", "tmp"
+append :linked_dirs, "log", "public/system", "tmp/sockets", "tmp/storage", "tmp/", "tmp/cache", "tmp/pids"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
