44eb6c53fed4263fca4063f201acbbfd7639ebb9
andreyuhai <burak.kaymakci@bil.omu.edu.tr>
2020-02-23 13:13:04 +0300

    Added sidekiq tasks

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,3 +59,17 @@ server "andreyuhai.com", user: "deploy", roles: %w{app db web}
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
+
+namespace :sidekiq do
+  task :quiet do
+    on roles(:app) do
+      puts capture("pgrep -f 'sidekiq' | xargs kill -TSTP")
+    end
+  end
+
+  task :restart do
+    on roles(:app) do
+      execute :sudo, :systemctl, :restart, 'sidekiq'
+    end
+  end
+end
