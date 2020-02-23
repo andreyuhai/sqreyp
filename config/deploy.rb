64f001298f64457c44e13d5fd3a9e9e1a1f63708
andreyuhai <burak.kaymakci@bil.omu.edu.tr>
2020-02-23 13:12:51 +0300

    Added sidekiq tasks

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,3 +42,7 @@ append :linked_dirs, "log", "public/system", "tmp"
 
 # Uncomment the following to require manually verifying the host key before first deploy.
 # set :ssh_options, verify_host_key: :secure
+
+after 'deploy:starting', 'sidekiq:quiet'
+after 'deploy:reverted', 'sidekiq:restart'
+after 'deploy:published', 'sidekiq:restart'
