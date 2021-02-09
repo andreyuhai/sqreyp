950f697835700b7acf63a26202774de72fc7fb75
andreyuhai <burakaymakci@gmail.com>
2021-02-09 12:36:38 +0100

    Changed mail subject, Scrape ~> Sqreyp

--- app/mailers/scrape_notifier_mailer.rb
+++ app/mailers/scrape_notifier_mailer.rb
@@ -3,13 +3,13 @@ class ScrapeNotifierMailer < ApplicationMailer
 
   def send_scrape_request_email(scrape_request)
     mail(to: scrape_request.email,
-         subject: 'Scrape request has been received!')
+         subject: 'Sqreyp request has been received!')
   end
 
   def send_scrape_result_email(scrape_request)
     @download_link = Rails.application.routes.url_helpers.polymorphic_url(scrape_request.scrape_result.file,
                                                                           subdomain: 'sqreyp')
     mail(to: scrape_request.email,
-         subject: 'Scrape result is ready!')
+         subject: 'Sqreyp result is ready!')
   end
 end
